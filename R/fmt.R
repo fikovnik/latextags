ee3d9df499506dbff9fe39e393c01bac20e35830
Filip Krikava <krikava@gmail.com>
2019-06-27 15:50:33 +0200

    Rename approx to oom, add size and documentation

--- R/fmt.R
+++ R/fmt.R
@@ -1,3 +1,18 @@
+#' Formats given value.
+#'
+#' The `x` value is formatted according to its class with an additional
+#' `prefix` and `suffix` prepended and appended respectively.
+#'
+#' @param x the value to be formatted
+#' @param prefix the string to be prepended
+#' @param suffix the string to be appended
+#' @param ... parameters to be passed to the actual formatter. The number
+#'     formatter accepts `floor` and `ceiling` to indicate that given value
+#'     should be rounded down or up to a full integer before formatting or
+#'     `digits` to indicate the number of digits the number should be rounded
+#'     to.
+#'
+#' @export
 fmt <- function(x, prefix="", suffix="", ...) {
   if (is.null(x)) {
     NULL
@@ -20,47 +35,86 @@ fmt <- function(x, prefix="", suffix="", ...) {
   format(x, big.mark=",", trim=TRUE)
 }
 
-.fmt.double <- function(x, floor=FALSE, ceiling=FALSE, digits=1) {
+.fmt.double <- function(x, digits=1, floor=FALSE, ceiling=FALSE) {
   if (floor) x <- floor(x)
   if (ceiling) x <- ceiling(x)
-  format(round(x, digits), big.mark=",", trim=TRUE)
+
+  prettyNum(round(x, digits), big.mark=",")
 }
 
 .fmt.percent <- function(x, floor=FALSE, ceiling=FALSE, digits=0) {
   fmt(as.double(x), suffix="%", floor=floor, ceiling=ceiling, digits=digits)
 }
 
-.fmt.approx <- function(x, floor=FALSE, ceiling=FALSE, digits=1) {
-  symbol <- attr(x, "symbol")
-  fmt(as.double(x), suffix=symbol, floor=floor, ceiling=ceiling, digits=digits)
+.fmt.num_with_suffix <- function(x, floor=FALSE, ceiling=FALSE, digits=1) {
+  suffix <- attr(x, "suffix")
+  fmt(as.double(x), suffix=suffix, floor=floor, ceiling=ceiling, digits=digits)
 }
 
+#' Converts given value to a percent representation.
+#'
+#' It multiples the value by 100 and adds the `%` suffix attribute.
+#'
+#' @param x the value to convert
+#'
+#' @export
 percent <- function(x) {
-  class(x) <- "percent"
-  x*100
+    x <- x*100
+    class(x) <- "num_with_suffix"
+    attr(x, "suffix") <- "%"
+    x
 }
 
-approx <- function(x, magnitute=NULL, symbol=NULL) {
-    OMV <- c(0, 3, 6, 9, 12)
-    OMN <- c("", "K", "M", "G", "T")
+scale_with_suffix <- function(x, base, suffixes) {
+    stopifnot(all(base > 0))
+    stopifnot(length(suffixes) > 0)
+    stopifnot(is.character(suffixes))
 
-    if (is.null(magnitute)) {
-        magnitute <- floor(log10(abs(x)))
-        magnitute <- floor(magnitute / 3) * 3
-        magnitute[magnitute > OMV[length(OMV)]] <- OMV[length(OMV)]
+    scale <- if (length(base) == 1) {
+        base^(1:length(suffixes))
+    } else {
+        stopifnot(length(base) == length(x))
+        base
     }
+
+    suffixes <- suffixes
+    magnitute <- floor(log(abs(x), base))
+    magnitute[magnitute > max(scale)] <- max(scale)
+
+    stopifnot(which.max(scale) <= length(suffixes))
 
     magnitute <- as.integer(magnitute)
+    suffix <- sapply(magnitute, function(x) suffixes[x + 1])
 
-    if (is.null(symbol)) {
-        symbol <- sapply(magnitute / 3 + 1, function(x) OMN[x])
-    } else {
-        stopifnot(length(symbol) %in% c(1, length(magnitute)))
-    }
-
-    d <- 10^magnitute
-    v <- x/d
-    class(v) <- "approx"
-    attr(v, "symbol") <- symbol
+    d <- base^magnitute
+    v <- x / d
+    class(v) <- "num_with_suffix"
+    attr(v, "suffix") <- suffix
     v
+}
+
+#' Scale the value to its closest order of magnitude
+#'
+#' It uses the base of 1000.
+#'
+#' @param x the value to scale
+#' @param suffixes are the suffixes to be add. Each corresponds to the exponent
+#'     of the base.
+#'
+#' @export
+oom <- function(x, suffixes=c("", "K", "M", "G", "T")) {
+    scale_with_suffix(x, base=1000, suffixes=suffixes)
+}
+
+#' Scale the value to its closest order of magnitude in the size of bytes.
+#'
+#' It uses the base of 1024.
+#'
+#' @param x the value to scale
+#' @param units are the suffixes to be add. Each corresponds to the exponent
+#'     of the base.
+#'
+#' @export
+size <- function(x, units=c("B", "kB", "MB", "GB", "TB")) {
+    scale_with_suffix(x, base=1024, suffixes=units)
 }
--- tests/testthat/test-fmt.R
+++ tests/testthat/test-fmt.R
@@ -13,6 +13,7 @@ test_that("fmt formats a number", {
     expect_equal(fmt(1.238, digits=2), "1.24")
     expect_equal(fmt(1.234, floor=T), "1")
     expect_equal(fmt(1.234, ceiling=T), "2")
+    expect_equal(fmt(c(123, 123.456), digits=1), c("123", "123.5"))
 })
 
 test_that("fmt formats vector", {
@@ -23,10 +24,14 @@ test_that("fmt formats percent", {
     expect_equal(fmt(percent(c(1.1234, 0.9959)), digits=1), c("112.3%", "99.6%"))
 })
 
-test_that("fmt formats approximations", {
-    expect_equal(fmt(approx(c(1234, 123, 1234567, 123456))), c("1.2K", "123.0", "1.2M", "123.5K"))
+test_that("fmt formats orders of magnitude", {
+    expect_equal(fmt(oom(c(1234, 123, 1234567, 123456))), c("1.2K", "123", "1.2M", "123.5K"))
 })
 
-test_that("fmt formats negative approximations", {
-    expect_equal(fmt(approx(-1 * c(1234, 123, 1234567, 123456))), c("-1.2K", "-123.0", "-1.2M", "-123.5K"))
+test_that("fmt formats negative orders of magnitude", {
+    expect_equal(fmt(oom(-1 * c(1234, 123, 1234567, 123456))), c("-1.2K", "-123", "-1.2M", "-123.5K"))
+})
+
+test_that("fmt formats size", {
+    expect_equal(fmt(size(c(123, 1024*123, 1024*1024*123, 1024*1024, 12354))), c("123B", "123kB", "123MB", "1MB", "12.1kB"))
 })
